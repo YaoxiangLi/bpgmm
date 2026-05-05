0461de0efacd2158d594122a5f76dfa8b0d489d0
Yaoxiang Li <liyaoxiang@outlook.com>
2026-05-05 00:59:55 -0400

    Update package source to 1.1.1

--- R/hyperParamClass.R
+++ R/hyperParamClass.R
@@ -7,10 +7,6 @@
 #' @slot ggamma A numeric value
 #' @slot bbeta A numeric value
 #'
-#' @examples
-#' new("Hparam", alpha1=1, alpha2=2, bbeta=3, delta=4, ggamma=5)
-#'
-#' @export
 setClass(
   "Hparam",
   slots = c(
@@ -30,13 +26,12 @@ setClass(
 )
 
 
-#' @export
 setValidity("Hparam", function(object) {
   if (object@alpha1 < 0 |
-      object@alpha2 < 0 |
-      object@delta  < 0 |
-      object@ggamma < 0 |
-      object@bbeta  < 0) {
+    object@alpha2 < 0 |
+    object@delta < 0 |
+    object@ggamma < 0 |
+    object@bbeta < 0) {
     "Hyperparameter should be non-negative!"
   }
 })
--- R/utils.R
+++ R/utils.R
@@ -1,42 +1,27 @@
-#' Tool for vector to matrix
-#'
-#' @param ZOneDim a vector.
-#' @param m the number of cluster.
-#' @param n sample size.
-#' @return adjacency matrix
-#'
-#' @export
+#' (internal)
+#' @noRd
 getZmat <- function(ZOneDim, m, n) {
   Zmat <- matrix(NA, m, n)
   for (i in 1:m) {
-    Zmat[i,] = as.numeric(ZOneDim == i)
+    Zmat[i, ] <- as.numeric(ZOneDim == i)
   }
   Zmat
 }
 
-#' Log scale ratio calculation
-#'
-#' @param deno denominator.
-#' @param nume numerator.
-#' @return result of ratio
-#'
-#' @export
+#' (internal)
+#' @noRd
 calculateRatio <- function(deno, nume) {
   ## deno nume both in log scale
-  maxNume = max(nume)
-  transDeno = deno - maxNume
-  transNume = nume - maxNume
-  res = exp(transDeno) / (sum(exp(transNume)))
+  maxNume <- max(nume)
+  transDeno <- deno - maxNume
+  transNume <- nume - maxNume
+  res <- exp(transDeno) / (sum(exp(transNume)))
   res
 }
 
 
-#' Convert list of string to vector of string
-#'
-#' @param stringList list of string
-#' @return vector of string
-#'
-#' @export
+#' (internal)
+#' @noRd
 listToStrVec <- function(stringList) {
   for (i in 1:length(stringList)) {
     stringList[[i]] <-
@@ -47,103 +32,148 @@ listToStrVec <- function(stringList) {
 
 
 
-#' likelihood
-#'
-#' @param thetaYList thetaYList
-#' @param ZOneDim ZOneDim
-#' @param qqVec qqVec
-#' @param muBar muBar
-#' @param X X
-#'
-#' @return likelihood value for parameters thetaYList
-#' @examples
-#'
-#' \donttest{
-#' url <- paste0("https://github.com/lzyacht/bpgmm-examples/",
-#' "blob/master/data/CalculateProposalLambda.RData?raw=true")
-#'
-#'
-#' download.file(url, destfile= "CalculateProposalLambda.RData", mode = "wb")
-#' load("CalculateProposalLambda.RData")
-#' likelihood(thetaYList, ZOneDim,qVec,muBar, X)
-#' }
-#' @export
-#'
-likelihood = function(thetaYList, ZOneDim, qqVec, muBar, X){
-
-  m = length(qqVec)
-  n = ncol(X)
-
+#' (internal)
+#' @noRd
+likelihood <- function(thetaYList, ZOneDim, qqVec, muBar, X) {
+  m <- length(qqVec)
+  n <- ncol(X)
+  xvec <- c()
   ## 2.1: X
-  xVal = 0
-  for(i in 1:n){
+  xVal <- 0
+  for (i in 1:n) {
     ## for subject i
-    k = ZOneDim[i]
+    k <- ZOneDim[i]
 
-    meanx = thetaYList@lambda[[k]] %*% thetaYList@Y[[k]][,i] + c(thetaYList@M[[k]])
-    varx =  thetaYList@psy[[k]]
+    # if(is.vector(X[, ZOneDim == k])){
+    #   meanx = X[, ZOneDim == k]
+    # }else{
+    #   meanx = apply(X[, ZOneDim == k], 1, mean)
+    # }
 
-    xVal = xVal + dmvnorm(x = X[,i], mean = meanx, sigma = varx, log = T)
+    meanx <- thetaYList@lambda[[k]] %*% thetaYList@Y[[k]][, i] + c(thetaYList@M[[k]])
+    varx <- thetaYList@psy[[k]]
+    xvec[i] <- (mvtnorm::dmvnorm(x = X[, i], mean = meanx, sigma = varx, log = T))
+    xVal <- xVal + mvtnorm::dmvnorm(x = X[, i], mean = meanx, sigma = varx, log = T)
   }
 
   xVal
+  # xvec
 }
 
-#' sumerizeZ
-#'
-#' @param Zlist list of Z matrix
-#' @param index index of Zlist
-#'
-#'
-#' @return count mode of Z matrix
-#'
-#'
-#'
-sumerizeZ = function(Zlist, index = 1:length(Zlist)){
-  sampleSize = length(Zlist[[1]])
-  res = c()
-  for(i in 1:sampleSize){
-    temp = c()
-    for(j in index){
-      temp[j] = Zlist[[j]][i]
+#' (internal)
+#' @noRd
+sumerizeZ <- function(Zlist, index = 1:length(Zlist)) {
+  sampleSize <- length(Zlist[[1]])
+  res <- c()
+  for (i in 1:sampleSize) {
+    temp <- c()
+    for (j in index) {
+      temp[j] <- Zlist[[j]][i]
     }
-    res[i] = getmode(temp)
+    res[i] <- getmode(temp)
   }
   return(res)
 }
 
 
-#' getmode
-#'
-#' @param v vec of numeric values
-#' @return mode of the vec
-#'
-getmode <- function(v){
-  v = v[!is.na(v)]
+
+#' (internal)
+#' @noRd
+getmode <- function(v) {
+  v <- v[!is.na(v)]
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
 
-#' calculateVarList
-#'
-#' @param psyList list of psy
-#' @param lambdaList list of lambda
-#'
-#' @return list of covariance matrix
-#'
-calculateVarList = function(psyList,lambdaList){
-  m = length(psyList)
-  varList = list()
-  for(i in 1:m){
-    tempCov = psyList[[i]] + lambdaList[[i]]%*%t(lambdaList[[i]])
-    varList[[i]] = tempCov
+#' (internal)
+#' @noRd
+calculateVarList <- function(psyList, lambdaList) {
+  m <- length(psyList)
+  varList <- list()
+  for (i in 1:m) {
+    tempCov <- psyList[[i]] + lambdaList[[i]] %*% t(lambdaList[[i]])
+    varList[[i]] <- tempCov
   }
   return(varList)
 }
 
 
+#' (internal)
+#' @noRd
+clearCurrentThetaYlist <- function(thetaYList, clusInd, mMax) {
+  resThetaYList <- thetaYList
+
+  for (i in 1:mMax) {
+    if (clusInd[i] == 1) {
+      resThetaYList@tao[i] <- thetaYList@tao[i]
+      resThetaYList@psy[[i]] <- thetaYList@psy[[i]]
+      resThetaYList@M[[i]] <- thetaYList@M[[i]]
+      resThetaYList@lambda[[i]] <- thetaYList@lambda[[i]]
+      resThetaYList@Y[[i]] <- thetaYList@Y[[i]]
+    } else {
+      resThetaYList@tao[i] <- 0
+      resThetaYList@psy[[i]] <- NA
+      resThetaYList@M[[i]] <- NA
+      resThetaYList@lambda[[i]] <- NA
+      resThetaYList@Y[[i]] <- NA
+    }
+  }
+  return(resThetaYList)
+}
+
+
+#' (internal)
+#' @noRd
+combineClusterPara <- function(oldList, newList, ind) {
+  resList <- oldList
+  resList@tao <- oldList@tao * (1 - newList@tao)
+  resList@tao[ind] <- newList@tao
+
+  resList@psy[ind] <- newList@psy
+  resList@M[ind] <- newList@M
+  resList@lambda[ind] <- newList@lambda
+  resList@Y[ind] <- newList@Y
+  return(resList)
+}
 
 
 
 
+#' (internal)
+#' @noRd
+getIndThetaY <- function(thetaYList, Ind) {
+  new("ThetaYList",
+    tao = thetaYList@tao[Ind],
+    psy = thetaYList@psy[Ind],
+    M = thetaYList@M[Ind],
+    lambda = thetaYList@lambda[Ind],
+    Y = thetaYList@Y[Ind]
+  )
+}
+
+#' (internal)
+#' @noRd
+getRemovedIndThetaY <- function(thetaYList, Ind) {
+  res <- thetaYList
+
+  res@tao[Ind] <- NA
+  res@tao <- res@tao / sum(res@tao, na.rm = T)
+  res@psy[[Ind]] <- NA
+  res@M[[Ind]] <- NA
+  res@lambda[[Ind]] <- NA
+  res@Y[[Ind]] <- NA
+
+  res
+}
+
+#' (internal)
+#' @noRd
+changeConstraintFormat <- function(strNum) {
+  res <- ""
+  res <- gsub(pattern = "0", replacement = "U", x = strNum)
+  res <- gsub(pattern = "1", replacement = "C", x = res)
+  res <- gsub(pattern = "\\(", replacement = "", x = res)
+  res <- gsub(pattern = "\\)", replacement = "", x = res)
+  return(res)
+}
